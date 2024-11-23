df0a1ff239527f3bf7496df8ff06559e7982e7d5
Timur Beteev <timurbeteev@Timurs-MacBook-Air.local>
2024-11-23 13:59:58 +0300

    Added logic for Vitya's work and rest schedule

--- index.js
+++ index.js
@@ -24,6 +24,44 @@ if (dif_in_days % 4 === 0 || (dif_in_days - 1) % 4 === 0) {
 
 });
 
+
+bot.command('checkWeek', (ctx) => {
+    var now = new Date();
+    var cur_date = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+    
+    //пятницу и субботу работает 22.11 23.11 
+    
+    var workday_1 = new Date(2024, 10, 22)
+    
+    
+    var dif_in_days = (cur_date.getTime() - workday_1.getTime())/ (1000 * 3600 * 24);
+    
+    if (dif_in_days % 4 === 0) {
+        console.log('Vitya works today');
+        ctx.reply('Сегодня Витя работает');
+    } else {
+        console.log('Vitya doesn\'t work today');
+        ctx.reply('Сегодня Витя отдыхает');
+    }
+    
+    if ((dif_in_days + 1) % 4 === 0) {
+        console.log('Vitya works tomorrow');
+        ctx.reply('Завтра Витя работает');
+    } else {
+        console.log('Vitya doesn\'t work tomorrow');
+        ctx.reply('Завтра Витя отдыхает');
+    }
+    
+    if ((dif_in_days + 2) % 4 === 0) {
+        console.log('Vitya works the day after tomorrow');
+        ctx.reply('Послезавтра Витя работает');
+    } else {
+        console.log('Vitya doesn\'t work the day after tomorrow');
+        ctx.reply('Послезавтра Витя отдыхает');
+    }
+    
+    });
+
 bot.launch();
 
 
